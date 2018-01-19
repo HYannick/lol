384b0e9d6abb1c8883efa349427d623076f32196
Yannick Houssin <y.houssin@live.fr>
2018-01-19 12:54:12 +0100

    dl

--- controllers/uploadCtrl.js
+++ controllers/uploadCtrl.js
@@ -25,67 +25,42 @@ module.exports = {
     }
 
     const audioOutput = path.resolve(__dirname, `audio/${title}.mp3`);
-    const mainOutput = path.resolve(__dirname, `./${title}.mp3`);
-
     ytdl.getInfo(url, (err, info) => {
       if (err) throw err;
       if (info.length_seconds > 900) {
         res.status(500).json({error: 'video too long bud\' :( '})
       } else {
         res.json({processing: 'processing ...'});
-        const video = ytdl(url, {filter: 'audioonly'});
-        let starttime;
-        video.pipe(fs.createWriteStream(mainOutput));
-        video.once('response', () => {
-          starttime = Date.now();
-        });
-        video.on('progress', (chunkLength, downloaded, total) => {
-          const floatDownloaded = downloaded / total;
-          const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
-          const data = {
-            id,
-            percent: (floatDownloaded * 100).toFixed(2),
-            estimated: (downloadedMinutes / floatDownloaded - downloadedMinutes).toFixed(2)
-          }
-          ioSocket.emit('downloadingVideo', data)
-        });
-        video.on('end', () => {
-          ffmpeg()
-            .input(mainOutput)
-            .format(args.format)
-            .audioCodec('libmp3lame')
-            .audioBitrate(args.bitrate)
-            .on('error', err => {
-              fs.unlink(audioOutput, err => {
-                if (err) console.error(err);
-                else console.log('Failed. File deleted =>', audioOutput);
-                ioSocket.emit('sendFileError', {id, status: 0})
-              });
+        ffmpeg()
+          .input(ytdl(url, {filter: 'audioonly'}))
+          .format(args.format)
+          .audioCodec('libmp3lame')
+          .audioBitrate(args.bitrate)
+          .on('error', err => {
+            fs.unlink(audioOutput, err => {
+              if (err) console.error(err);
+              else console.log('Failed. File deleted =>', audioOutput);
+              ioSocket.emit('sendFileError', {id, status: 0})
+            });
+          })
+          .on('progress', progress => {
+            const currentTime = moment.duration(progress.timemark).asSeconds();
+            const percent = (currentTime / info.length_seconds) * 100
+            console.log(`Status => ${percent}%`)
+            ioSocket.emit('downloading', {id, status: percent})
+          })
+          .on('end', () => {
+            fs.readFile(audioOutput, (err, file) => {
+              ioSocket.emit('sendFile', {id, status: 100, blob: file})
+              setTimeout(() => {
+                fs.unlink(audioOutput, err => {
+                  if (err) console.error(err);
+                  else console.log('Success. File deleted =>', audioOutput);
+                });
+              }, 2000);
             })
-            .on('progress', progress => {
-              const currentTime = moment.duration(progress.timemark).asSeconds();
-              const percent = (currentTime / info.length_seconds) * 100
-              console.log(`Status => ${percent}%`)
-              ioSocket.emit('downloading', {id, status: percent})
-            })
-            .on('end', () => {
-              fs.readFile(audioOutput, (err, file) => {
-                ioSocket.emit('sendFile', {id, status: 100, blob: file})
-                setTimeout(() => {
-                  fs.unlink(audioOutput, err => {
-                    if (err) console.error(err);
-                    else console.log('Success. File deleted =>', audioOutput);
-                  });
-                  fs.unlink(mainOutput, err => {
-                    if (err) console.error(err);
-                    else console.log('Success. File deleted =>', audioOutput);
-                  });
-                }, 2000);
-              })
-            })
-            .save(audioOutput)
-        });
-
+          })
+          .save(audioOutput)
       }
     })
   }
